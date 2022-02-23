2886a497f66cf6c9cc0681999905b6d7615bd5ee
Stepan Ponomarev <stponomarevw@gmail.com>
2022-02-23 12:18:28 +0300

    configure format

---
+++ README.md
@@ -0,0 +1,7 @@
+## Configure project before making commits
+
+* Install [pre-commit hook](https://github.com/a1exsh/google-java-format-git-pre-commit-hook) so code will be in strict format in repo.
+* Import intellij-java-google-style.xml into File→Settings→Editor→Code Style to fix import order
+
+#### Optional:
+* Install [google-java-format](https://github.com/google/google-java-format) plugin from IntelliJ Idea Marketplace
--- src/test/java/function/ArctgTests.java
+++ src/test/java/function/ArctgTests.java
@@ -1,25 +1,22 @@
 package function;
 
-import function.MathCalculator;
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 public class ArctgTests {
-    private MathCalculator calculator = new MathCalculator();
+  private MathCalculator calculator = new MathCalculator();
 
-    @Test
-    void testArctg() {
-        System.out.printf(calculator.arctg(3, 0.0001).toString());
-    }
+  @Test
+  void testArctg() {
+    System.out.printf(calculator.arctg(3, 0.0001).toString());
+  }
 
-    @ParameterizedTest
-    @CsvSource({"0,0.001,0",
-            "1,0.0001, 0.78539",
-            "3,0.0001, 1.2490"})
-    void testAround0(double x, double precision, double expectedResult) {
-        assertTrue(Math.abs(calculator.arctg(x, precision) - expectedResult) < precision);
-    }
+  @ParameterizedTest
+  @CsvSource({"0,0.001,0", "1,0.0001, 0.78539", "3,0.0001, 1.2490"})
+  void testAround0(double x, double precision, double expectedResult) {
+    assertTrue(Math.abs(calculator.arctg(x, precision) - expectedResult) < precision);
+  }
 }
