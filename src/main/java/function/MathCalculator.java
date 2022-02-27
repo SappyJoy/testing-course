965d0a7c3ff14f4b1628a9b849b87cd97a64f6d8
Daniel <rosdanas@gmail.com>
2022-02-27 14:40:57 +0300

    some junit tests

--- src/main/java/function/MathCalculator.java
+++ src/main/java/function/MathCalculator.java
@@ -1,8 +1,12 @@
 package function;
 
 public class MathCalculator {
-  public Double arctg(double x, double precision) {
-    //        validateParameters(precision);
+
+  public Double arctg(double x, double precision) throws Exception {
+    validateParameters(x, precision);
+    if (Math.abs(x) == 1) {
+      return x * 0.7853981633974483;
+    }
     int n = 2;
     double prevValue = x;
     double result = calculateArctgTailorOnStep(x, n++, prevValue);
@@ -13,11 +17,15 @@ public class MathCalculator {
     return calculateArctgTailorOnStep(x, n, result);
   }
 
-  private void validateParameters(double precision) {
-    if (precision <= 0) throw new IllegalArgumentException("Precision can't be less or equal 0");
-  }
-
   private double calculateArctgTailorOnStep(double x, int n, double prevSum) {
     return prevSum + Math.pow(-1, (n - 1)) * Math.pow(x, 2 * n - 1) / (2 * n - 1);
+  }
+
+  private void validateParameters(double x, double precision) throws Exception {
+    if (precision <= 0)
+      throw new IllegalArgumentException("Precision can't be less or equal 0");
+    if (Math.abs(x) > 1)
+      throw new IllegalArgumentException(
+          "Module of x can't be more than 1, because of the way of calculating");
   }
 }
--- src/test/java/function/ArctgTests.java
+++ src/test/java/function/ArctgTests.java
@@ -2,21 +2,75 @@ package function;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 public class ArctgTests {
+
   private MathCalculator calculator = new MathCalculator();
+  private double DEFAULT_PRECISION = Math.pow(10D,-15);
 
   @Test
-  void testArctg() {
-    System.out.printf(calculator.arctg(3, 0.0001).toString());
+  void testArctg() throws Exception {
+    System.out.println(calculator.arctg(1, DEFAULT_PRECISION).toString());
   }
 
+  @DisplayName("Testing precision of calculating method in interval [0,1]")
   @ParameterizedTest
-  @CsvSource({"0,0.001,0", "1,0.0001, 0.78539", "3,0.0001, 1.2490"})
-  void testAround0(double x, double precision, double expectedResult) {
-    assertTrue(Math.abs(calculator.arctg(x, precision) - expectedResult) < precision);
+  @CsvSource({"0,0",
+      "0.1, 0.09966865249116204",
+      "0.2, 0.19739555984988078",
+      "0.3, 0.2914567944778671",
+      "0.4, 0.3805063771123649",
+      "0.5, 0.4636476090008061",
+      "0.6, 0.5404195002705842",
+      "0.7, 0.6107259643892086",
+      "0.8, 0.6747409422235527",
+      "0.9, 0.7328151017865066",
+      "1  , 0.7853981633974483",
+  })
+  void testPrecision(double x, Double expectedResult) throws Exception {
+    assertTrue(Math.abs(calculator.arctg(x, DEFAULT_PRECISION) - expectedResult) <= DEFAULT_PRECISION);
   }
+
+  @DisplayName("Testing precision of calculating negative values in interval [-1,1]")
+  @ParameterizedTest
+  @CsvSource({
+      "-0.1, -0.09966865249116204",
+      "-0.3, -0.2914567944778671",
+      "-0.5, -0.4636476090008061",
+      "-0.7, -0.6107259643892086",
+      "-0.9, -0.7328151017865066",
+      "-1  , -0.7853981633974483",})
+  void testNegative(double x, Double expectedResult) throws Exception {
+    assertTrue(Math.abs(calculator.arctg(x, DEFAULT_PRECISION) - expectedResult) <= DEFAULT_PRECISION);
+  }
+
+  @DisplayName("Testing throwing exception while x more than 1")
+  @Test
+  void testMoreThen1() throws Exception {
+    assertThrows(IllegalArgumentException.class, () -> calculator.arctg(1.1, DEFAULT_PRECISION));
+  }
+
+  @DisplayName("Testing throwing exception while x less than -1")
+  @Test
+  void testLessThenMinus1() throws Exception {
+    assertThrows(IllegalArgumentException.class, () -> calculator.arctg(-1.1, DEFAULT_PRECISION));
+  }
+
+  @DisplayName("Testing throwing exception while precision equals 0")
+  @Test
+  void testPrecision0() throws Exception {
+    assertThrows(IllegalArgumentException.class, () -> calculator.arctg(-1.1, 0));
+  }
+
+  @DisplayName("Testing throwing exception while precision less then 0")
+  @Test
+  void testPrecisionLess0() throws Exception {
+    assertThrows(IllegalArgumentException.class, () -> calculator.arctg(-1.1, -1));
+  }
+
+
 }
