909698559744dc8cc68fe9660b622f13e1fff62f
Belinracle <61329697+Belinracle@users.noreply.github.com>
2022-03-12 21:57:15 +0300

    added Shell sort alogorithm
    added Shell sort alogorithm

---
+++ src/test/java/sorting/ShellSortTests.java
@@ -0,0 +1,42 @@
+package sorting;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+public class ShellSortTests {
+
+  ShellSort sorter = new ShellSort();
+
+  @DisplayName("Test correct work of sort algorithm")
+  @ParameterizedTest
+  @MethodSource("provideArraysForSortTests")
+  void sortTests(int[] arrayToSort, int[] expectedResult) {
+    System.out.println(
+        "Test sorting with arrayToSort: " + Arrays.toString(arrayToSort) + " and expectedResult: "
+            + Arrays.toString(expectedResult));
+
+    sorter.sort(arrayToSort);
+    assertArrayEquals(arrayToSort, expectedResult);
+  }
+
+  private static Stream<Arguments> provideArraysForSortTests() {
+    List<Arguments> arguments = new ArrayList<>();
+    int[][] arrays = {new int[]{9, 8, 7, 6, 5, 4, 3, 2, 1},
+        new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 0},
+        new int[]{-8, -4, -765, -256, -2, 234525, 26, 6, 2, 2},
+        new int[]{2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2}};
+    for (int[] array : arrays) {
+      arguments.add(Arguments.of(array, Arrays.stream(array).sorted().toArray()));
+    }
+    return arguments.stream();
+  }
+}
